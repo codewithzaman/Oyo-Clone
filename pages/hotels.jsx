31241bc04dc76883b0d09ae76f300ac9473d1a03
codewithzaman <naderzaman50@gmail.com>
2024-03-17 00:14:08 +0500

    Create hotels.jsx

---
+++ pages/hotels.jsx
@@ -0,0 +1,24 @@
+import Header1 from "@/components/Header1";
+import Hotel from "@/components/Hotel";
+import React from "react";
+
+const Hotels = () => {
+  return (
+    <>
+      <Header1 />
+      <div className=" m-5">
+        <Hotel />
+        <Hotel />
+        <Hotel />
+        <Hotel />
+        <Hotel />
+        <Hotel />
+        <Hotel />
+        <Hotel />
+        <Hotel />
+      </div>
+    </>
+  );
+};
+
+export default Hotels;
